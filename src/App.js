4a36603807b24029f63d4259ad818d46eb67ab8e
PhilipL <honesty1997@gmail.com>
2021-12-10 02:26:12 +0800

    commit before eject

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ class App extends React.PureComponent {
     const scene = new Three.Scene();
     const camera = new Three.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new Three.WebGLRenderer();
+    
+    camera.position.set(0, 100, 0);
+    camera.lookAt(0, 0, 0);
+  
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0xffffff, 1);
     document.querySelector("#main").appendChild(renderer.domElement);
 
-    const geometry = new Three.BoxGeometry();
-    const material = new Three.MeshBasicMaterial( { color: 0x00ff00 } );
+    const geometry = new Three.IcosahedronGeometry(20, 4);
+    const material = new Three.MeshBasicMaterial( { color: 0xb7ff00, wireframe: true });
     const cube = new Three.Mesh( geometry, material );
     scene.add( cube );
-
-    camera.position.z = 5;
 
     const animate = function () {
       requestAnimationFrame( animate );
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
+      cube.rotation.z += 0.01;
       renderer.render( scene, camera );
     };
 
